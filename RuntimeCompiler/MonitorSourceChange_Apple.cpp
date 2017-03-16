652d0bde71ee35691d27baae57ce6fd7ddf4842d
Akito Nozaki <akito@netbrains.com>
2017-03-15 17:08:23 -0700

    Getting build environment for Mac and iOS setup.

--- RuntimeCompiler/MonitorSourceChange_Apple.cpp
+++ RuntimeCompiler/MonitorSourceChange_Apple.cpp
@@ -2,14 +2,50 @@
 // Created by Akito Nozaki on 3/13/17.
 //
 
+#include <thread>
+
+#ifdef __APPLE__
+#    ifdef TARGET_OS_IPHONE
+#        include <MobileCoreServices/MobileCoreServices.h>
+#    elif TARGET_OS_MAC
+#        include <CoreServices/CoreServices.h>
+#    endif
+#endif
+
 #include "MonitorSourceChange.h"
 
-class MonitorSourceChange::Private {
+class MonitorSourceChangePlatform {
 
 };
 
+class MonitorSourceChange_Apple: public MonitorSourceChangePlatform {
+public:
+    MonitorSourceChange_Apple() : mThread( &MonitorSourceChange_Apple::run, this ) {
+    }
+
+    void start() {
+
+    }
+
+    void stop() {
+
+    }
+
+private:
+    void run() {
+        mRunLoop = CFRunLoopGetCurrent();
+        CFRunLoopRun();
+    }
+
+    std::thread mThread;
+    CFRunLoopRef mRunLoop;
+};
+
 void MonitorSourceChange::startWatchingDirectory() {
+    // is there a thread already running?
+
     // need to create workThread
     // mRunLoop = CFRunLoopGetCurrent()
     //
 }
+
--- RuntimeCompiler/main.cpp
+++ RuntimeCompiler/main.cpp
@@ -2,10 +2,6 @@
 #include "MonitorSourceChange.h"
 
 int main() {
-    MonitorSourceChange monitorSourceChange;
-    monitorSourceChange.addWatchDirectory(".");
-    monitorSourceChange.start();
-
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
